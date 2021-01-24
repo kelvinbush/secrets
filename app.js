349accf3d60d43573000821e9ebe1fea64e9581d
Kelvin Bush <kelybush@gmail.com>
2021-01-24 11:58:59 +0300

    add gitignore

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields:["password"]});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
