51f36dcb86ce06dc2d563a97ed0a55b1cd587769
Kelvin Bush <kelybush@gmail.com>
2021-01-24 07:30:12 +0300

    initial set up

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('*/css', express.static('public/css'));
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
