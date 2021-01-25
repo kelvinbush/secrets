b0ac35dafda3b89bd0e17dade42247a3c560e11f
Kelvin Bush <kelybush@gmail.com>
2021-01-25 04:52:11 +0300

    add dependencies

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 const app = express();
